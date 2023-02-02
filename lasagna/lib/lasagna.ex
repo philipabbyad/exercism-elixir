ecb4bb619c28b83c51600ef9b64989a9fd9f361a
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2023-02-02 14:27:56 -0700

    Completed task 2

--- lasagna/lib/lasagna.ex
+++ lasagna/lib/lasagna.ex
@@ -5,6 +5,9 @@ defmodule Lasagna do
   end
 
   # Please define the 'remaining_minutes_in_oven/1' function
+  def remaining_minutes_in_oven(minutes) do
+    expected_minutes_in_oven() - minutes
+  end
 
   # Please define the 'preparation_time_in_minutes/1' function
 
