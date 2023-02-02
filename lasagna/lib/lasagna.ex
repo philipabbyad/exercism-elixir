109f1d6a171af7781cf437b6381d3d198db5dd3d
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2023-02-02 14:40:48 -0700

    Used one-line syntax for 2 functions. Removed comments.

--- lasagna/lib/lasagna.ex
+++ lasagna/lib/lasagna.ex
@@ -1,27 +1,17 @@
 defmodule Lasagna do
-  # Please define the 'expected_minutes_in_oven/0' function
-  def expected_minutes_in_oven do
-    40
-  end
+  def expected_minutes_in_oven, do: 40
 
-  # Please define the 'remaining_minutes_in_oven/1' function
   def remaining_minutes_in_oven(minutes) do
     expected_minutes_in_oven() - minutes
   end
 
-  # Please define the 'preparation_time_in_minutes/1' function
   def preparation_time_in_minutes(num_layers) do
     num_layers * 2
   end
 
-  # Please define the 'total_time_in_minutes/2' function
   def total_time_in_minutes(num_layers, minutes) do
     preparation_time_in_minutes(num_layers) + minutes
   end
 
-  # Please define the 'alarm/0' function
-  def alarm do
-    "Ding!"
-  end
-  
+  def alarm, do: "Ding!"
 end
