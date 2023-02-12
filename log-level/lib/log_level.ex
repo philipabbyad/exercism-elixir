8ab69914f29bc31474c7f257b44c8bada2e15747
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2023-02-12 16:50:32 -0700

    Completed task 1

--- log-level/lib/log_level.ex
+++ log-level/lib/log_level.ex
@@ -1,6 +1,18 @@
 defmodule LogLevel do
   def to_label(level, legacy?) do
-    # Please implement the to_label/2 function
+    cond do
+      level == 0 or level == 5 ->
+        cond do
+          legacy? -> :unknown
+          level == 0 -> :trace
+          level == 5 -> :fatal
+        end
+      level == 1 -> :debug
+      level == 2 -> :info
+      level == 3 -> :warning
+      level == 4 -> :error
+      true -> :unknown
+    end
   end
 
   def alert_recipient(level, legacy?) do
