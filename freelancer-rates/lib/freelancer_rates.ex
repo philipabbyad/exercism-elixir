4f8c56d91091d3e4b78a43db348325de080bec1e
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2023-02-12 16:06:52 -0700

    Completed exercise - all tests pass.

--- freelancer-rates/lib/freelancer_rates.ex
+++ freelancer-rates/lib/freelancer_rates.ex
@@ -1,17 +1,17 @@
 defmodule FreelancerRates do
   def daily_rate(hourly_rate) do
-    # Please implement the daily_rate/1 function
+    8.0 * hourly_rate
   end
 
   def apply_discount(before_discount, discount) do
-    # Please implement the apply_discount/2 function
+    before_discount * ((100 - discount) / 100)
   end
 
   def monthly_rate(hourly_rate, discount) do
-    # Please implement the monthly_rate/2 function
+    trunc(Float.ceil((22 * apply_discount(daily_rate(hourly_rate), discount)), 0))
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
-    # Please implement the days_in_budget/3 function
+    Float.floor((budget / apply_discount(daily_rate(hourly_rate), discount)), 1)
   end
 end
